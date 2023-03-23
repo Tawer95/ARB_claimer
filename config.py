ea5d090847073038a68a781923d13f0c6d69e89a
Tawer95 <73060710+Tawer95@users.noreply.github.com>
2023-03-23 12:30:37 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+prv_key_array = [
+  'YOUR PRIVATE KEY'
+  'NEW PRIVATE KEY'
+]
+
+destination_address_array = [
+    "YOUR DESTINATION ADDRESS",
+    "SECOND DESTINATION ADDRESS",
+    # destination addresses here
+    # must be same length as private keys array
+]
